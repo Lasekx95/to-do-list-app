52f89ee2ded20bf8638c0db08bf2bda96d3c0c15
Dustin <ddemers1995@hotmail.com>
2022-12-16 14:29:40 -0800

    added more JS

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ const saveInd = document.getElementById("saveIndex");
 
 let todoArray = [];
 
+addTaskButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let todo = localStorage.getItem("todo");
+    if (todo === null) {
+        todoArray = [];
+    } 
+    else {
+        todoArray.JSON.parse(todo);
+    }
+    todoArray.push(text.value);
+    text.value = "";
+    localStorage.setItem("todo", JSON.stringify(todoArray));
+    displayTodo();
+});
+
+function displayTodo() {
+    let todo = localStorage.getItem("todo");
+    if (todo === null) {
+        todoArray = [];
+    }
+    else {todoArray = JSON.parse(todo);
+    }
+    
+}
